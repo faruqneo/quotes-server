523f744cb05e62f63eaf1581fb7579139d138c07
faruqneo <faruq_khan@yahoo.com>
2020-04-13 11:00:34 +0530

    trim

--- src/quotes/model/quotes.model.ts
+++ src/quotes/model/quotes.model.ts
@@ -3,10 +3,12 @@ import * as mongoose from 'mongoose';
 export const QuoteSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: [true, 'Please enter the title']
+        required: [true, 'Please enter the title'],
+        trim: true
     },
     author: {
         type: String,
-        required: [true, 'Please enter the author']
+        required: [true, 'Please enter the author'],
+        trim: true
     }
 })
